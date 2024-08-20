816a203e5e69ec3300d506fc91db33589131a3a9
Stepan Krousky <stepan.krousky@seznam.cz>
2024-08-20 21:03:47 +0200

    fix error

--- lib/db.ts
+++ lib/db.ts
@@ -9,9 +9,7 @@ export async function connectToDatabase() {
   if (isConnected) return console.log('MongoDB is already connected')
 
   try {
-    await mongoose.connect(process.env.MONGODB_URL, {
-      dbName: 'devflow',
-    })
+    await mongoose.connect(process.env.MONGODB_URL, { socketTimeoutMS: 30000 })
 
     isConnected = true
     console.log('MongoDB is connected')
